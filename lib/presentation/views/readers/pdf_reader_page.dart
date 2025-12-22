2d4ddb364eb2fa83696e0ced2faf6d33e896861d
ardavan8102 <eskandariardavan81@gmail.com>
2025-12-22 22:07:52 +0330

    on progress -> pdf viewer

--- lib/presentation/views/readers/pdf_reader_page.dart
+++ lib/presentation/views/readers/pdf_reader_page.dart
@@ -1,6 +1,6 @@
-
 import 'package:dotbook/presentation/widgets/appbar/appbar_with_return_icon.dart';
 import 'package:flutter/material.dart';
+import 'package:pdfx/pdfx.dart';
 
 class PdfReaderPage extends StatefulWidget {
   const PdfReaderPage({
@@ -18,14 +18,61 @@ class PdfReaderPage extends StatefulWidget {
 
 class _PdfReaderPageState extends State<PdfReaderPage> {
 
-  int? pages = 0;
-  int? currentPage = 0;
+  late PdfController _pdfController;
+  final int _initPage = 1;
+
+  int? currentPage;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _pdfController = PdfController(
+      document: PdfDocument.openFile(widget.filePath),
+      initialPage: _initPage
+    );
+  }
+
+
+  @override
+  void dispose() {
+    _pdfController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWithReturnIcon(title: widget.fileName),
-      body: Text('Test'),
+      body: PdfView(
+        controller: _pdfController,
+        builders: PdfViewBuilders<DefaultBuilderOptions>(
+          options: const DefaultBuilderOptions(),
+          documentLoaderBuilder: (_) => const Center(child: CircularProgressIndicator()),
+          pageLoaderBuilder: (_) => const Center(child: CircularProgressIndicator()),
+          pageBuilder: _pageBuilder,
+        ),
+      ),
+    );
+  }
+
+
+  PhotoViewGalleryPageOptions _pageBuilder(
+    BuildContext context,
+    Future<PdfPageImage> pageImage,
+    int index,
+    PdfDocument document,
+  ) {
+    return PhotoViewGalleryPageOptions(
+      imageProvider: PdfPageImageProvider(
+        pageImage,
+        index,
+        document.id
+      ),
+      minScale: PhotoViewComputedScale.contained * 1,
+      maxScale: PhotoViewComputedScale.contained * 2,
+      initialScale: PhotoViewComputedScale.contained * 1.0,
+      heroAttributes: PhotoViewHeroAttributes(tag: '${document.id}-$index')
     );
   }
 }
